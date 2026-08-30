41040e9fb924441412eeda69ba51e07edc91b142
Oscar Echegaray <oechegaray@360creative.pe>
2026-08-30 04:38:24 -0500

    feat(views): add Blade templates for app dashboard and static web pages

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>Sin Excusas - ERP</title>
+    @vite('resources/js/app.ts')
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
